502c84b7ed790672affd3a2ae982fca8e87b57fc
luxiaoxuan <luxiaoxuan1984@qq.com>
2016-05-03 18:01:13 +0800

    ..

---
+++ src/main/java/lxx/demo/springwebmvc/controllers/WelcomeController.java
@@ -0,0 +1,18 @@
+package lxx.demo.springwebmvc.controllers;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("time", new Date());
+		model.put("message", "Go Go");
+		return "welcome";
+	}
+}
