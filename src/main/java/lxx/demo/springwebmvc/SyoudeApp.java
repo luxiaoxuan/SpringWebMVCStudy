2139e40a6eeaa3df8c1bd7bba1eb1f7b886cae87
luxiaoxuan <luxiaoxuan1984@qq.com>
2016-05-03 17:00:35 +0800

    Initialized.

--- src/main/java/lxx/demo/springwebmvc/SyoudeApp.java
+++ src/main/java/lxx/demo/springwebmvc/SyoudeApp.java
@@ -1,17 +1,18 @@
 package lxx.demo.springwebmvc;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
-public class SyoudeApp {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SyoudeApp.class, args);
-	}
+public class SyoudeApp extends SpringBootServletInitializer  {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SyoudeApp.class);		
+	}
 }
